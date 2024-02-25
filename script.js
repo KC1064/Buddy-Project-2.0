08e9a363202bbf4f808cd763c5ff60277ff90ee8
KC1064 <kiron08072003@gmail.com>
2024-02-25 11:24:08 +0530

    Update: Added Time blocks

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const kiron = [
-  ["DBMS", "Operating System", "Economics", "DAA", "Free", "Math IV", "Free", "AJP/AI"],
+  ["DBMS", "OS", "Economics", "DAA", "Free", "Math IV", "Free", "AJP/AI"],
   ["Free", "DBMS", "DBMS Lab", "DBMS Lab", "Free", "Math IV", "Free", "Free"],
-  ["Operating System", "AJP/AI", "DBMS", "Math IV", "Free", "DAA", "DBMS Lab", "DBMS Lab"],
-  ["Free", "Operating System", "Economics", "Free", "Free", "DAA", "DAA Lab", "DAA Lab"],
-  ["Operating System Lab", "Operating System Lab", "DBMS", "Free", "Math IV", "DAA", "Economics", "AJP/AI"]
+  ["OS", "AJP/AI", "DBMS", "Math IV", "Free", "DAA", "DBMS Lab", "DBMS Lab"],
+  ["Free", "OS", "Economics", "Free", "Free", "DAA", "DAA Lab", "DAA Lab"],
+  ["OS Lab", "OS Lab", "DBMS", "Free", "Math IV", "DAA", "Economics", "AJP/AI"]
 ];
 
 const mrinal = [
@@ -13,8 +13,34 @@ const mrinal = [
   ["Free", "Operating System", "Economics", "Free", "Free", "DAA", "DAA Lab", "DAA Lab"],
   ["Operating System Lab", "Operating System Lab", "DBMS", "Free", "Math IV", "DAA", "Economics", "AJP/AI"]
 ];
+const selectedValue = document.getElementById("friends").value;
+// function displaySelectedValue() {
+//   document.getElementById("selected").innerText = `Selected Value: ${selectedValue}`;
+// }
+const today = new Date();
+function printTT(){
+  const day = today.getDay();
+  switch (day){
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+       return 'Friday';
+    case 6:
+      return 'Saturday';
+    default:
+        return 'Error! Invalid Day of the Week';
+  }
 
-function displaySelectedValue() {
-  const selectedValue = document.getElementById("friends").value;
-  document.getElementById("selected").innerText = `Selected Value: ${selectedValue}`;
+  switch(selectedValue){
+    case 'Kiron':
+  }
+
 }
